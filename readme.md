663df8a6c1ce9dc2ba6b68157950a928dc889c21
cjlaborde <cjlaborde@gmail.com>
2018-12-07 19:31:39 -0400

    Setup production webpack build

---
+++ readme.md
@@ -0,0 +1,52 @@
+# Git Commands
+
+git init - Create a new repo
+
+git status - View the changes to your projects
+
+git add - Add files to staging arare
+
+git commit - Creates a new commit with files from staging area
+
+git log - View recent commits
+
+## Find SSH KEYS
+
+ls -a ~/.ssh
+
+https://help.github.com/articles/connecting-to-github-with-ssh/
+
+## Check is ssh-agent running
+
+ eval "$(ssh-agent -s)"
+
+## Add Key
+
+ ssh-add ~/.ssh/id_rsa
+
+## Basic connection to github services 
+
+ssh -T git@github.com
+
+## Add git using ssh
+
+git remote add origin git@github.com:cjlaborde/expensify-app.git
+
+## check if it running
+
+git remote -v
+
+## push to git
+
+-u create assosiation between our local and github repository you only need to use it once
+then type remote name =origin  and branch = master
+git push -u orgin master
+
+git push --set-upstream origin master
+
+## WebPack Production 
+
+  "scripts": {
+    "build": "webpack",
+yarn run build 
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,59 @@
  // entry -> output
  const path = require('path')
+ // webpack plugins require you ro add extra settings in plugin array here as ExtractTextPlugin did
+ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
- module.exports = {
-    mode: 'development',
-    entry: './src/app.js',
-    // entry: './src/playground/destructuringArray.js',
-    // entry: './src/playground/hoc.js',
-    // entry: './src/playground/redux-expensify.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'main.js'
-    },
-    module: {
-        rules: [{
-          loader: 'babel-loader',
-          // regular expression to search for files that ends$ with js
-          test: /\.js$/,
-          exclude: /node_modules/
-        }, {
-            // s?css means it allows both scss and css files
-            test: /\.s?css$/,
-            // use allow us to specify group of loaders
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
+ module.exports = (env) => {
+     const isProduction = env === 'production'
+     const CSSExtract = new ExtractTextPlugin('styles.css')
+
+    return {
+        mode: 'development',
+        entry: './src/app.js',
+        // entry: './src/playground/hoc.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'main.js'
+        },
+        module: {
+            rules: [{
+            loader: 'babel-loader',
+            // regular expression to search for files that ends$ with js
+            test: /\.js$/,
+            exclude: /node_modules/
+            }, {
+                // s?css means it allows both scss and css files
+                test: /\.s?css$/,
+                // use allow us to specify group of loaders
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+
+                        }
+                    ]
+                })
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
     }
-}
+ }
+
 
 // loader
